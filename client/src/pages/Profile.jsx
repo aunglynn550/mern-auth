29f43f2232cdb207e381b8378604f1d2b4092d6d
aunglynn550 <aunglynn550@gmail.com>
2024-01-20 17:03:09 +0630

    add routes and create pages ii

---
+++ client/src/pages/Profile.jsx
@@ -0,0 +1,5 @@
+import React from "react"
+
+export default function Profile(){
+  return <h1 className="text-red-500">Profile</h1>
+}
---
+++ client/src/pages/SignIn.jsx
@@ -0,0 +1,5 @@
+import React from "react"
+
+export default function SignIn(){
+  return <h1 className="text-red-500">Signin</h1>
+}
